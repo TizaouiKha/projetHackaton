21b2277aa93cae416422766ef00ef163ef35fbad
Khalil Tizaoui <62132130+TizaouiKha@users.noreply.github.com>
2024-02-05 14:45:21 +0100

    creation of project Back

---
+++ projetHackatonBack/src/main/java/com/example/projetHackatonBack/ProjetHackatonBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetHackatonBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetHackatonBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetHackatonBackApplication.class, args);
+	}
+
+}
---
+++ projetHackatonBack/src/test/java/com/example/projetHackatonBack/ProjetHackatonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetHackatonBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetHackatonBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
