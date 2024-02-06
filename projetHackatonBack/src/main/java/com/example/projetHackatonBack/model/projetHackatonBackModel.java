c6a5cfd680c94df4b51ba0928a60eef269768fbc
Khalil Tizaoui <62132130+TizaouiKha@users.noreply.github.com>
2024-02-06 09:47:03 +0100

    fix

--- projetHackatonBack/src/main/java/com/example/projetHackatonBack/model/projetHackatonBackModel.java
+++ projetHackatonBack/src/main/java/com/example/projetHackatonBack/model/projetHackatonBackModel.java
@@ -1,8 +1,46 @@
-
 package com.example.projetHackatonBack.model;
 
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
-public class projetHackatonBackModel {
+@Entity
+public class projetHackatonBackModel implements Serializable{
+    @Id
+    private Long id;
+    private String name;
+
+    public projetHackatonBackModel(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
