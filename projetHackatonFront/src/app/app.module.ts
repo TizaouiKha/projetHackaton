d5bf529709fc1a9d7d558b1d98624c905fd26db0
khali <khalimmamode@hotmail.com>
2024-02-07 13:39:19 +0100

    rajout du bouton/card/them

--- projetHackatonFront/src/app/app.module.ts
+++ projetHackatonFront/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNavList } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent, SafeHtmlPipe } from './components/accueil/accueil.component';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatNavList} from '@angular/material/list';
 import { SidebarToggleComponent } from './components/sidebar-toggle/sidebar-toggle.component';
-import {MatButtonModule} from '@angular/material/button';
+
 
 
 
@@ -33,8 +37,14 @@ import {MatButtonModule} from '@angular/material/button';
     NoopAnimationsModule,
     MatNavList,
     MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatCardModule,
+
   ],
   providers: [],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
